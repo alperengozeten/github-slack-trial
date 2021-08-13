04f3cdd6f43edbf82c528da33ce20d002b9b326b
HP <alperengozeten@gmail.com>
2021-08-13 10:25:58 +0300

    start-v2

--- src/main/java/test/TestClass.java
+++ src/main/java/test/TestClass.java
@@ -3,6 +3,6 @@ package test;
 public class TestClass {
     public static void main(String[] args) {
         System.out.println("This is a test class");
-        System.out.println("This is a test classsss");
+        System.out.println("This is a test classssss");
     }
 }
